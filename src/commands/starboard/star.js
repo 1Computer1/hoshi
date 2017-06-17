5f48a3f0a6a3f7c98f56321ff654b8aa6cb25cd2
1Computer1 <mineox100@gmail.com>
2017-06-17 17:52:29 -0400

    Start star and unstar commands

---
+++ src/commands/starboard/star.js
@@ -0,0 +1,37 @@
+const { Command } = require('discord-akairo');
+const Logger = require('../../util/Logger');
+
+class StarCommand extends Command {
+	constructor() {
+		super('star', {
+			aliases: ['star'],
+			category: 'starboard',
+			channelRestriction: 'guild',
+			args: [
+				// Indices are swapped in order to process channel first.
+				{
+					id: 'channel',
+					index: 1,
+					type: 'textChannel',
+					default: message => message.channel
+				},
+				{
+					id: 'message',
+					index: 0,
+					type: (word, message, { channel }) => {
+						if (!word) return null;
+						// eslint-disable-next-line prefer-promise-reject-errors
+						return channel.fetchMessage(word).catch(() => Promise.reject());
+					}
+				}
+			]
+		});
+	}
+
+	exec(message, { channel, message: msg }) {
+		// Temporary, implement starring later.
+		Logger.log(`${channel.name}: ${msg.content}`);
+	}
+}
+
+module.exports = StarCommand;
