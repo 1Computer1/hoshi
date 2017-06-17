2760165f6452922b3226a7a2ecfb70f6b473816b
vzwGrey <thegreylp@googlemail.com>
2017-06-17 19:57:42 +0200

    Add some logging

--- hoshi.js
+++ hoshi.js
@@ -1,5 +1,13 @@
 const HoshiClient = require('./src/struct/HoshiClient');
 const config = require('./config.json');
+const Logger = require('./src/util/Logger');
 
 const client = new HoshiClient(config);
+
+client.on('ready', () => Logger.info(`${client.user.tag} is ready to serve!`))
+	.on('disconnect', () => Logger.warn('Connection lost...'))
+	.on('reconnect', () => Logger.info('Attempting to reconnect...'))
+	.on('error', Logger.error)
+	.on('warn', Logger.warn);
+
 client.start();
