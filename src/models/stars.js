552bfd466fe3b8dbd9e9417e57538cebe5f2e427
vzwGrey <thegreylp@googlemail.com>
2017-07-28 18:47:38 +0200

    Create star db model

---
+++ src/models/stars.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+
+const { db } = require('../struct/Database');
+
+const Star = db.define('stars', {
+	messageID: {
+		type: Sequelize.STRING,
+		primaryKey: true,
+		allowNull: false
+	},
+	channelID: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	guildID: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	starCount: {
+		type: Sequelize.INTEGER,
+		allowNull: false,
+		defaultValue: 1
+	},
+	starredBy: {
+		type: Sequelize.ARRAY(Sequelize.STRING), // eslint-disable-line new-cap
+		allowNull: false
+	}
+});
+
+module.exports = Star;
