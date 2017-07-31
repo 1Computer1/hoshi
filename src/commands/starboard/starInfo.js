fe7b4ec2cd3e067f77f0e8ce02f90a0c4b13e516
1Computer1 <mineox100@gmail.com>
2017-07-31 16:59:55 -0400

    Add star info command

---
+++ src/commands/starboard/starInfo.js
@@ -0,0 +1,63 @@
+const { Command } = require('discord-akairo');
+const { MessageEmbed } = require('discord.js');
+
+class StarInfoCommand extends Command {
+	constructor() {
+		super('starInfo', {
+			aliases: ['starInfo', 'star-info'],
+			category: 'starboard',
+			channelRestriction: 'guild',
+			args: [
+				// Indices are swapped in order to process channel first.
+				{
+					id: 'channel',
+					index: 1,
+					type: 'textChannel',
+					default: message => message.channel
+				},
+				{
+					id: 'message',
+					index: 0,
+					type: (word, message, { channel }) => {
+						if (!word) return null;
+						// eslint-disable-next-line prefer-promise-reject-errors
+						return channel.fetchMessage(word).catch(() => Promise.reject());
+					}
+				}
+			]
+		});
+	}
+
+	exec(message, { message: msg }) {
+		const starboard = this.client.starboards.get(message.guild.id);
+		if (!starboard.channel) {
+			return message.util.reply('There isn\'t a starboard channel to use.');
+		}
+
+		const star = starboard.stars.get(msg.id);
+		if (!star) {
+			return message.util.reply('That message has not been starred.');
+		}
+
+		const emoji = star.starredBy.length < 3
+			? '⭐'
+			: star.starredBy.length < 5
+				? '🌟'
+				: star.starredBy.length < 10
+					? '✨'
+					: '🌌';
+
+		const embed = new MessageEmbed()
+			.setColor(0xFFAC33)
+			.addField('Author', msg.author, true)
+			.addField('Channel', msg.channel, true)
+			.addField('Starrers', star.starredBy.map(id => this.client.users.get(id)).join(', '))
+			.setThumbnail(msg.author.displayAvatarURL({ size: 1024 }))
+			.setTimestamp(msg.createdAt)
+			.setFooter(`${emoji} ${star.starredBy.length} | ${msg.id}`);
+
+		return message.util.send({ embed });
+	}
+}
+
+module.exports = StarInfoCommand;
