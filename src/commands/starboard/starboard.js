5351dc92ecf5f48cadd669ad3833a64e4ad70523
1Computer1 <mineox100@gmail.com>
2017-07-31 22:25:34 -0400

    Add confirmation prompt for starboard change

--- src/commands/starboard/starboard.js
+++ src/commands/starboard/starboard.js
@@ -15,19 +15,44 @@ class StarCommand extends Command {
 						start: msg => `${msg.author} **::** What channel would you like to use as the starboard?`,
 						retry: msg => `${msg.author} **::** Please provide a valid text channel.`
 					}
+				},
+				{
+					id: 'confirm',
+					match: 'none',
+					type: (word, message, { channel }) => {
+						const oldID = this.client.settings.get(message.guild, 'starboardChannelID');
+						if (oldID && oldID !== channel.id) {
+							// Yes or yea, or anything less
+							if (/^y(?:e(?:a|s)?)?$/i.test(word)) return 'yes';
+							// Nope or nay, or anything less
+							if (/^n(?:op?e?|ay?)?$/i.test(word)) return 'no';
+							return null;
+						}
+
+						return 'first';
+					},
+					prompt: {
+						start: msg => `${msg.author} **::** Are you sure you want to delete the previous starboard? (y/n)`,
+						retry: () => ''
+					}
 				}
 			]
 		});
 	}
 
-	async exec(message, { channel }) {
-		if (!channel) return message.util.reply('You must provide a starboard channel.');
+	async exec(message, { channel, confirm }) {
+		if (confirm === 'no') {
+			return message.util.reply('Starboard change has been cancelled.');
+		}
 
 		const oldID = this.client.settings.get(message.guild, 'starboardChannelID');
-		await this.client.settings.set(message.guild, 'starboardChannelID', channel.id);
 
-		if (oldID && oldID !== channel.id) {
-			await this.client.starboards.get(channel.guild.id).destroy();
+		if (['yes', 'first'].includes(confirm)) {
+			await this.client.settings.set(message.guild, 'starboardChannelID', channel.id);
+
+			if (oldID && oldID !== channel.id) {
+				await this.client.starboards.get(channel.guild.id).destroy();
+			}
 		}
 
 		return message.util.reply(`Starboard channel has been set to ${channel}`);
