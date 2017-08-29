05f95f209b30ba7a5955517375de81e9e41ef60e
vzwGrey <thegreylp@googlemail.com>
2017-08-29 08:52:06 +0200

    If `content` is array join by newline

--- src/util/Extensions.js
+++ src/util/Extensions.js
@@ -3,12 +3,12 @@ const { Message } = require('discord.js');
 
 Object.defineProperty(CommandUtil.prototype, 'reply', {
 	value: function reply(content, options) {
-		return this.send(`${this.message.author} **::** ${content}`, options);
+		return this.send(`${this.message.author} **::** ${Array.isArray(content) ? content.join('\n') : content}`, options);
 	}
 });
 
 Object.defineProperty(Message.prototype, 'reply', {
 	value: function reply(content, options) {
-		return this.channel.send(`${this.author} **::** ${content}`, options);
+		return this.channel.send(`${this.author} **::** ${Array.isArray(content) ? content.join('\n') : content}`, options);
 	}
 });
