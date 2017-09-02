5fe88d275567e29b91d6d16b1d3fe394fdbd3cc3
1Computer1 <mineox100@gmail.com>
2017-09-02 15:49:19 -0400

    Use arg prompting for yes/no

--- src/commands/general/reset.js
+++ src/commands/general/reset.js
@@ -17,41 +17,45 @@ class ResetCommand extends Command {
 						retry: msg => [
 							`${msg.author} **::** You did not choose a valid reset mode.`,
 							'Choose one of `stars`, `rep` or `all`'
-						].join(' ')
+						]
+					}
+				},
+				{
+					id: 'confirm',
+					match: 'none',
+					type: word => {
+						if (!word) return null;
+
+						// Yes, yea, ye, or y.
+						if (/^y(?:e(?:a|s)?)?$/i.test(word)) return 'yes';
+						return 'no';
+					},
+					prompt: {
+						start: (msg, { mode }) => `${msg.author} **::** ${{
+							stars: 'Are you sure you want to reset all stars on this server? (y/N)',
+							rep: 'Are you sure you want to reset all reputation points on this server? (y/N)',
+							all: 'Are you sure you want to reset all stars and reputation points on this server? (y/N)'
+						}[mode]}`,
+						retry: () => ''
 					}
 				}
 			]
 		});
+	}
 
-		this.confirmationMessages = {
-			stars: 'Are you sure you want to reset all stars on this server? (__y__es/__n__o)',
-			rep: 'Are you sure you want to reset aa repuation points on this server? (__y__es/__n__o)',
-			all: 'Are you sure you want to reset all stars and reputation points on this server? (__y__es/__n__o)'
-		};
+	async exec(message, { mode, confirm }) {
+		if (confirm === 'no') {
+			return message.util.send('Reset has been cancelled.');
+		}
 
-		this.successMessages = {
+		if (mode === 'stars' || mode === 'all') await this.client.starboards.get(message.guild.id).destroy();
+		if (mode === 'rep' || mode === 'all') await Reps.destroy({ where: { guildID: message.guild.id } });
+
+		return message.util.send(`${message.author} **::** ${{
 			stars: 'Successfully removed all starred messages on this server.',
 			rep: 'Successfully removed all reputation points on this server.',
 			all: 'Successfully removed all starred messages and reputation points on this server.'
-		};
-	}
-
-	async exec(message, { mode }) {
-		message.channel.send(`${message.author} **::** ${this.confirmationMessages[mode]}`);
-
-		const confirmation = await message.channel.awaitMessages(m => m.author.id === message.author.id, {
-			maxMatches: 1,
-			time: 10000,
-			errors: ['time']
-		}).then(messages => messages.first().content);
-		if (['yes', 'y'].includes(confirmation.toLowerCase())) {
-			if (mode === 'stars' || mode === 'all') await this.client.starboards.get(message.guild.id).destroy();
-			if (mode === 'rep' || mode === 'all') await Reps.destroy({ where: { guildID: message.guild.id } });
-
-			return message.util.send(`${message.author} **::** ${this.successMessages[mode]}`);
-		} else {
-			return message.util.send('Aborting...');
-		}
+		}[mode]}`);
 	}
 }
 
