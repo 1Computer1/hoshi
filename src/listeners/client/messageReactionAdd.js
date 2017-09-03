f10d7d41c30b699292251b7063fc953a701f49cd
1Computer1 <mineox100@gmail.com>
2017-09-03 15:01:51 -0400

    Move perm check for starring via reaction

--- src/listeners/client/messageReactionAdd.js
+++ src/listeners/client/messageReactionAdd.js
@@ -14,16 +14,14 @@ class MessageReactionAddListener extends Listener {
 		if (!reaction.message.guild) return;
 
 		if (reaction.emoji.name === '⭐') {
-			if (!reaction.message.channel.permissionsFor(this.client.user).has('MANAGE_MESSAGES')) {
-				reaction.message.channel.send(`${user} **::** I'm missing \`Manage Messages\` to star that message in this channel.`);
-				return;
-			}
-
 			const starboard = this.client.starboards.get(reaction.message.guild.id);
 			const error = await starboard.add(reaction.message, user);
 
 			if (error) {
-				await reaction.remove(user);
+				if (reaction.message.channel.permissionsFor(this.client.user).has('MANAGE_MESSAGES')) {
+					await reaction.remove(user);
+				}
+
 				if (reaction.message.channel.permissionsFor(this.client.user).has('SEND_MESSAGES')) {
 					reaction.message.channel.send(`${user} **::** ${error}`);
 				}
