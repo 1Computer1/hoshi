c9b9fcfafb9a5d5fc2be65ce308cf630f8f9cabd
1Computer1 <mineox100@gmail.com>
2017-09-10 02:00:05 -0400

    Ignore reaction remove errors

--- src/listeners/client/messageReactionAdd.js
+++ src/listeners/client/messageReactionAdd.js
@@ -19,7 +19,7 @@ class MessageReactionAddListener extends Listener {
 
 			if (error) {
 				if (reaction.message.channel.permissionsFor(this.client.user).has('MANAGE_MESSAGES')) {
-					await reaction.remove(user);
+					await reaction.remove(user).catch(() => null);
 				}
 
 				if (reaction.message.channel.permissionsFor(this.client.user).has('SEND_MESSAGES')) {
