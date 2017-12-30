88f041635769c597d2c48345a53877ef8a103e14
1Computer1 <mineox100@gmail.com>
2017-12-29 21:36:15 -0500

    Add settings command

---
+++ src/commands/general/settings.js
@@ -0,0 +1,33 @@
+const { Command } = require('discord-akairo');
+
+class SettingsCommand extends Command {
+	constructor() {
+		super('settings', {
+			aliases: ['settings', 'view-settings'],
+			category: 'general',
+			channel: 'guild',
+			clientPermissions: ['EMBED_LINKS'],
+			description: { content: 'Displays the guild\'s current settings.' }
+		});
+	}
+
+	exec(message) {
+		const prefix = this.handler.prefix(message);
+		const starboard = this.client.starboards.get(message.guild.id);
+		const blacklist = this.client.settings.get(message.guild, 'blacklist', []);
+
+		const embed = this.client.util.embed()
+			.setColor(0xFFAC33)
+			.setTitle('Settings')
+			.setDescription([
+				`**Prefix**: \`${prefix}\``,
+				`**Starboard**: ${starboard ? starboard.channel : 'None'}`,
+				`**Threshold**: ${starboard ? starboard.threshold : 'None'}`,
+				`**Blacklist**: ${blacklist.join(', ') || 'None'}`
+			]);
+
+		return message.util.send({ embed });
+	}
+}
+
+module.exports = SettingsCommand;
