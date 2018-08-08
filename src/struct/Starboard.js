2fe4a8201e0a2dc758258cf8c4eea31a5d7f2cdb
vzwGrey <thegreylp@googlemail.com>
2018-08-08 13:13:06 +0200

    Add link to message in starboard embed

--- src/struct/Starboard.js
+++ src/struct/Starboard.js
@@ -339,6 +339,8 @@ class Starboard {
 			embed.addField('Message', content);
 		}
 
+		embed.addField('Jump To', message.url);
+
 		const attachment = Starboard.findAttachment(message);
 		if (attachment) {
 			embed.setImage(attachment);
