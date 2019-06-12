4739a3f7ec1bb2e027fecae2779352ed92402a59
Kayo <RyoshiKayo@users.noreply.github.com>
2019-06-12 05:58:28 -0700

    Fixed tabs

--- hoshi.js
+++ hoshi.js
@@ -8,17 +8,17 @@ const Sentry = require('@sentry/node');
 const client = new HoshiClient(config);
 
 if (config.sentryKey) {
-        Sentry.init({ dsn: config.sentryKey });
+	Sentry.init({ dsn: config.sentryKey });
 }
 
 client.on('disconnect', () => Logger.warn('Connection lost...'))
-        .on('reconnect', () => Logger.info('Attempting to reconnect...'))
-        .on('error', err => Logger.error(err))
-        .on('warn', info => Logger.warn(info));
+	.on('reconnect', () => Logger.info('Attempting to reconnect...'))
+	.on('error', err => Logger.error(err))
+	.on('warn', info => Logger.warn(info));
 
 client.start();
 
 process.on('unhandledRejection', err => {
-        Logger.error('An unhandled promise rejection occured');
-        Logger.stacktrace(err);
+	Logger.error('An unhandled promise rejection occured');
+	Logger.stacktrace(err);
 });
